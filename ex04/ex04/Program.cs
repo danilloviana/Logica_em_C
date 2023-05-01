2aefc19eb8c567be42a87ad71fac0520758b1906
Danillo Alves <danmarcosviana@gmail.com>
2023-05-01 13:08:35 -0300

    finalizando ex04

---
+++ ex04/ex04/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double preco, desconto, valorFinal;
+
+        Console.WriteLine("Digite o preço do produto:");
+        preco = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o desconto em %:");
+        desconto = double.Parse(Console.ReadLine());
+
+        valorFinal = preco - (preco * desconto / 100);
+
+        Console.WriteLine($"O valor final do produto é R$ {valorFinal:F2}");
+    }
+}
+
