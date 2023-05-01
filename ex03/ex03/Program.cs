6a8739dfb48aec6a5476b2fb1ac53a46243b49dd
Danillo Alves <danmarcosviana@gmail.com>
2023-05-01 12:10:43 -0300

    finalizando ex03

---
+++ ex03/ex03/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Faça o algoritmo (diagrama de blocos e português estruturado) para calcular o valor monetário do desconto (D), sendo que o preço do produto (PR) é fornecido pelo usuário e o desconto é de 5%. Apresentar o valor do desconto*/
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double PR, D;
+        Console.WriteLine("Digite o preço do produto:");
+        PR = double.Parse(Console.ReadLine());
+        D = PR * 0.05;
+        Console.WriteLine("O valor do desconto é de: " + D.ToString("C"));
+    }
+}
