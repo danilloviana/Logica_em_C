914dc9986d3b7354b57a78bd8a8d8e2b8589ed05
Danillo Alves <danmarcosviana@gmail.com>
2023-05-02 10:10:15 -0300

    finalizando ex05

---
+++ ex05/ex05/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        double preco, precof, desc;
+
+        Console.Write("Digite o preço original do produto: ");
+        preco = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite o preço com desconto do produto: ");
+        precof = double.Parse(Console.ReadLine());
+
+        desc = (1 - (precof / preco)) * 100;
+
+        Console.WriteLine("O desconto dado foi de " + desc.ToString("F2") + "%.");
+    }
+}
