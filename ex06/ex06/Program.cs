bd1b87825f609a5e028b3fe293c78f591e1b1988
Danillo Alves <danmarcosviana@gmail.com>
2023-05-02 10:15:50 -0300

    finalizando ex06

---
+++ ex06/ex06/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int anoAtual, anoNascimento, idade;
+
+        Console.WriteLine("Digite o ano atual: ");
+        anoAtual = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o ano de nascimento: ");
+        anoNascimento = int.Parse(Console.ReadLine());
+
+
+        idade = anoAtual - anoNascimento;
+
+
+        Console.WriteLine("A idade da pessoa é: " + idade + " anos.");
+    }
+}
