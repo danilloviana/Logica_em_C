f24d7020323e793d9d08f9706e755fd4353e45c6
Danillo Alves <danmarcosviana@gmail.com>
2023-05-04 13:22:55 -0300

    finalizando ex04

---
+++ lista_estruturas_de_decisao/ex04/ex04/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class MyProgram
+{
+    public static void Main(string[] args)
+    {
+        // 4. Divisão: Ler dois números e efetuar uma divisão, mas somente se o divisor for diferente de zero; quando isto ocorrer, é mostrada uma mensagem de erro apropriada.
+        double n1;
+        double n2;
+        double razao;
+
+        Console.WriteLine("Digite o primeiro número");
+        n1 = readValue();
+        Console.WriteLine("Digite o segundo número");
+        n2 = readValue();
+        if (n1 > 0 && n2 > 0)
+        {
+            razao = n1 / n2;
+            Console.WriteLine("O Resultado da divisão é " + razao);
+        }
+        else
+        {
+            Console.WriteLine("Esta operação não pode ser realizada");
+        }
+    }
+
+
+    private static double readValue()
+    {
+        double result;
+        while (!double.TryParse(Console.ReadLine(), out result)) ;
+        return result;
+    }
+}
