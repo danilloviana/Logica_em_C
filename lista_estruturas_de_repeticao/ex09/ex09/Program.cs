a876b10d324d906b4037df4b959699dfe5487f1a
Danillo Alves <danmarcosviana@gmail.com>
2023-05-08 12:26:53 -0300

    resolução de exercícios 08, 09 e 10

---
+++ lista_estruturas_de_repeticao/ex09/ex09/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Construa um programa que leia o conjunto de 20 números inteiros e mostre qual foi o maior e o menor valor fornecido.*/
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int max = int.MinValue; 
+        int min = int.MaxValue; 
+        for (int i = 0; i < 20; i++)
+        {
+            Console.Write("Digite um número inteiro: ");
+            int numero = int.Parse(Console.ReadLine());
+            if (numero > max)
+            { 
+                max = numero;
+            }
+            if (numero < min)
+            {
+                min = numero;
+            }
+        }
+        Console.WriteLine($"O maior número digitado foi {max} e o menor número digitado foi {min}.");
+    }
+}
---
+++ lista_estruturas_de_repeticao/ex10/ex10/Program.cs
@@ -0,0 +1,36 @@
+﻿/*Construa um programa que leia a quantidade (Q) e o preço (PR) de vários produtos diferentes, comprados por uma empresa, e apresente o total gasto por ela. O final da lista de produtos deverá ser indicado pelo usuário (escolha a maneira que preferir).*/
+
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        double totalGasto = 0;
+
+        while (true)
+        {
+            Console.Write("Digite a quantidade do produto (ou -1 para sair): ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            if (quantidade == -1)
+            {
+                break;
+            }
+
+            Console.Write("Digite o preço unitário do produto: ");
+            double preco = double.Parse(Console.ReadLine());
+
+            if (quantidade < 0 || preco < 0)
+            {
+                Console.WriteLine("Quantidade e preço não podem ser negativos.");
+            }
+            else
+            {
+                double totalProduto = quantidade * preco;
+                totalGasto += totalProduto;
+            }
+        }
+
+        Console.WriteLine("Total gasto pela empresa: R$ " + totalGasto);
+    }
+}
