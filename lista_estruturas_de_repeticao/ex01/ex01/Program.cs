45bdf53d73ab82cf69a34dd214ec98d4d009caec
Danillo Alves <danmarcosviana@gmail.com>
2023-05-08 10:31:23 -0300

    Criando branch lista03 e resolvendo primeiro exercício

---
+++ lista_estruturas_de_repeticao/ex01/ex01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double pesoTotal = 0;
+
+        for (int i = 1; i <= 25; i++)
+        {
+            Console.Write($"Digite o peso da caixa {i}: ");
+            double pesoCaixa = double.Parse(Console.ReadLine());
+            pesoTotal += pesoCaixa;
+        }
+
+        Console.WriteLine($"O peso total carregado pelo caminhão é {pesoTotal} kg.");
+    }
+}
