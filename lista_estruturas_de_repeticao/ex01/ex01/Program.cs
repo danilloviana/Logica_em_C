fb0f7470e2af5cf04cfe52fefe889cabc7632cdb
Danillo Alves <danmarcosviana@gmail.com>
2023-05-08 10:55:20 -0300

    resolução de exercícios 02, 03 e 04

--- lista_estruturas_de_repeticao/ex01/ex01/Program.cs
+++ lista_estruturas_de_repeticao/ex01/ex01/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 
 class Program
+/*Construa um programa que apresente o peso total que será carregado por um caminhão. Sabe-se que esse caminhão carrega 25 caixas, com pesos diferentes. Será entrada do programa o peso (P) de cada uma das caixas.*/
 {
     static void Main(string[] args)
     {
---
+++ lista_estruturas_de_repeticao/ex04/ex04/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+
+class Program
+/*Construa um programa que leia o número de horas trabalhadas diárias (NH) de um funcionário por um período de 30 dias (ele trabalhou todos os 30 dias) e apresente o salário bruto recebido por ele nesse período, sabendo que o valor do salário é R$ 10,00/hora trabalhada.*/
+
+{
+    static void Main(string[] args)
+    {
+        const double valorHora = 10.00;
+        double horasTrabalhadasTotal = 0;
+
+        for (int i = 1; i <= 30; i++)
+        {
+            Console.Write($"Digite o número de horas trabalhadas no dia {i}: ");
+            double horasTrabalhadasDiarias = double.Parse(Console.ReadLine());
+            horasTrabalhadasTotal += horasTrabalhadasDiarias;
+        }
+
+        double salarioBruto = horasTrabalhadasTotal * valorHora;
+
+        Console.WriteLine($"O salário bruto recebido pelo funcionário nos últimos 30 dias é: R$ {salarioBruto:F2}");
+    }
+}
